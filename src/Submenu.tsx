7906518e262e08d2fe6752ebcdb2551a7fa8970e
KONGKIAT888 <kong.workoffice@gmail.com>
2024-06-25 16:32:27 +0700

    minor change

--- src/Submenu.tsx
+++ src/Submenu.tsx
@@ -1,78 +1,54 @@
-import React from 'react';
+import React, { useCallback, useState, useRef, ReactNode, CSSProperties } from 'react';
 import classnames from 'classnames';
 import ContextMenuItem from './ContextMenuItem';
 
 interface SubmenuProps {
-    id: string;
-    children: React.ReactNode;
-    element: React.ReactNode;
+    children: ReactNode;
+    element: ReactNode;
     attributes?: React.HTMLAttributes<HTMLDivElement>;
     className?: string;
 }
 
-const Submenu: React.FC<SubmenuProps> = ({
-    id,
-    children,
-    element,
-    attributes = {},
-    className = ''
-}) => {
-    const [submenuStyle, setSubmenuStyle] = React.useState<React.CSSProperties>({});
-    const submenuEl = React.useRef<HTMLDivElement>(null);
-    const submenuItem = React.useRef<HTMLDivElement>(null);
+const Submenu: React.FC<SubmenuProps> = ({ children, element, attributes, className = '' }) => {
+    const [submenuStyle, setSubmenuStyle] = useState<CSSProperties | null>(null);
+    const submenuEl = useRef<HTMLDivElement>(null);
+    const submenuItem = useRef<HTMLDivElement>(null);
 
-    const calculateSubmenuPos = React.useCallback(() => {
-        const { innerHeight: windowInnerHeight, innerWidth: windowInnerWidth } = window;
-        const {
-            left: itemLeft, top: itemTop, width: itemWidth, height: itemHeight
-        } = submenuItem.current!.getBoundingClientRect();
-        const {
-            width: submenuWidth, height: submenuHeight
-        } = submenuEl.current!.getBoundingClientRect();
-        let style: React.CSSProperties = {
-            opacity: 1,
-            visibility: 'visible'
-        };
+    const calculateSubmenuPos = useCallback(() => {
+        if (submenuItem.current && submenuEl.current) {
+            const { innerHeight: windowInnerHeight, innerWidth: windowInnerWidth } = window;
+            const {
+                left: itemLeft, top: itemTop, width: itemWidth, height: itemHeight
+            } = submenuItem.current.getBoundingClientRect();
+            const {
+                width: submenuWidth, height: submenuHeight
+            } = submenuEl.current.getBoundingClientRect();
+            let style: CSSProperties = {
+                opacity: 1,
+                visibility: 'visible'
+            };
 
-        // Adjust for bottom overflow
-        if ((itemTop + submenuHeight + itemHeight) > windowInnerHeight) {
-            style = {
-                ...style,
-                top: 'inherit',
-                bottom: '0',
-                maxHeight: windowInnerHeight - itemTop - itemHeight,
-                overflowY: 'auto'
-            };
-        } else {
-            style = {
-                ...style,
-                top: 'inherit',
-                bottom: 'inherit',
-                maxHeight: 'inherit',
-                overflowY: 'inherit'
-            };
+            if ((itemTop + submenuHeight + itemHeight) > windowInnerHeight) {
+                style = {
+                    ...style,
+                    top: 'inherit',
+                    bottom: '0'
+                };
+            }
+            if ((itemLeft + submenuWidth + itemWidth) > windowInnerWidth) {
+                style = {
+                    ...style,
+                    left: 'inherit',
+                    right: '100%'
+                };
+            }
+
+            setSubmenuStyle(style);
         }
-
-        // Adjust for right overflow
-        if ((itemLeft + submenuWidth + itemWidth) > windowInnerWidth) {
-            style = {
-                ...style,
-                left: 'inherit',
-                right: '100%'
-            };
-        } else {
-            style = {
-                ...style,
-                left: itemWidth,
-                right: 'inherit'
-            };
-        }
-
-        setSubmenuStyle(style);
     }, []);
 
-    const hideSubmenu = React.useCallback(() => {
-        const style: React.CSSProperties = {
+    const hideSubmenu = useCallback(() => {
+        const style: CSSProperties = {
             opacity: 0,
             visibility: 'hidden'
         };
@@ -82,7 +58,7 @@ const Submenu: React.FC<SubmenuProps> = ({
 
     return (
         <div
-            className={classnames('submenu', className)}
+            className={classnames('submenu', ...className.split(' '))}
             onMouseOver={calculateSubmenuPos}
             onMouseOut={hideSubmenu}
             onFocus={() => null}
@@ -96,12 +72,12 @@ const Submenu: React.FC<SubmenuProps> = ({
             <div
                 className="submenu__item"
                 ref={submenuEl}
-                style={submenuStyle}
+                style={submenuStyle ?? undefined}
             >
                 {children}
             </div>
         </div>
     );
-};
+}
 
 export default Submenu;
